7f248b7a9ba8ce959695ef093dad9a5af0d41e23
yuunnn <wooxy610@icloud.com>
2020-05-22 21:57:40 +0800

    完成了cart回归树

--- algs/algorithms/tree.py
+++ algs/algorithms/tree.py
@@ -6,7 +6,7 @@ class Node:
         self.right = None
 
 
-class Btree:
+class BinaryTree:
 
     def __init__(self):
         self.root = None
@@ -34,23 +34,23 @@ class Btree:
         if node is None:
             return None
         _res.append(node.value)
-        Btree.pre_order_travel(node.left, _res)
-        Btree.pre_order_travel(node.right, _res)
+        BinaryTree.pre_order_travel(node.left, _res)
+        BinaryTree.pre_order_travel(node.right, _res)
 
     @staticmethod
     def in_order_travel(node, _res):
         if node is None:
             return None
-        Btree.in_order_travel(node.left, _res)
+        BinaryTree.in_order_travel(node.left, _res)
         _res.append(node.value)
-        Btree.in_order_travel(node.right, _res)
+        BinaryTree.in_order_travel(node.right, _res)
 
     @staticmethod
     def post_order_travel(node, _res):
         if node is None:
             return None
-        Btree.post_order_travel(node.left, _res)
-        Btree.post_order_travel(node.right, _res)
+        BinaryTree.post_order_travel(node.left, _res)
+        BinaryTree.post_order_travel(node.right, _res)
         _res.append(node.value)
 
     def travel(self, method="pre"):
@@ -63,10 +63,38 @@ class Btree:
             self.post_order_travel(self.root, _res)
         return _res
 
+    @property
+    def leaves(self):
+        _leaves = []
 
-t = Btree()
-L = [1, 2, 3, 4, 5]
-for i in L:
-    t.add(i)
-res = t.travel(method="pre")
-print(res)
+        def _travel(node, _leaves):
+            if node is None:
+                return None
+            if node.left is None and node.right is None:
+                _leaves.append(node.value)
+            if node.left is not None:
+                _travel(node.left, _leaves)
+            if node.right is not None:
+                _travel(node.right, _leaves)
+
+        _travel(self.root, _leaves)
+
+        return _leaves
+
+    @property
+    def height(self):
+
+        def _travel(node):
+            if node.value is None:
+                return 0
+            if node.left is None and node.right is None:
+                return 1
+            if node.left is None and node.right is not None:
+                return 1 + _travel(node.right)
+            if node.left is not None and node.right is None:
+                return 1 + _travel(node.left)
+            if node.left is not None and node.right is not None:
+                return 1 + max(_travel(node.left), _travel(node.right))
+
+        return _travel(self.root)
+
---
+++ algs/ml_algorithms/cart.py
@@ -0,0 +1,71 @@
+from algs.algorithms.tree import BinaryTree, Node
+import numpy as np
+from collections import namedtuple
+
+
+class Cart:
+    NodeValue = namedtuple("NodeValue", ['col', 'split', 'res_value'])
+
+    def __init__(self, max_depth, min_leave_samples=3, min_leave_samples_type=3):
+        self._tree = BinaryTree()
+        self._tree.root = Node(None)
+        self.max_depth = max_depth
+        self.min_leave_samples = min_leave_samples
+        self.min_leave_samples_type = min_leave_samples_type
+
+    def fit(self, x: np.ndarray, y: np.ndarray) -> None:
+
+        def _build(node, depth, _x, _y):
+
+            if depth == 0 or len(_x) <= self.min_leave_samples:
+                return
+
+            _index, _col, _split, _loss = None, None, None, +np.inf
+
+            for col in range(x.shape[1]):
+                if set(_x[:, col]).__len__() < self.min_leave_samples_type:
+                    continue
+                sort_indices = np.argsort(_x[:, col])
+                _x = _x[sort_indices]
+                _y = _y[sort_indices]
+
+                for val_index in range(1, len(_x) - 1):
+                    _y1 = _y[val_index:]
+                    _y2 = _y[:val_index]
+                    _tmp_loss = sum((_y1 - np.mean(_y1)) ** 2) + sum((_y2 - np.mean(_y2)) ** 2)
+                    if _tmp_loss < _loss:
+                        _col, _split, _loss = col, _x[val_index, col], _tmp_loss
+
+            node.value = self.NodeValue(col=_col, split=_split, res_value=np.mean(_y))
+            node.left = Node(None)
+            node.right = Node(None)
+            _index = _x[:, _col] < _split
+            _x1 = _x[_index]
+            _x2 = _x[~_index]
+
+            _y1 = _y[_index]
+            _y2 = _y[~_index]
+
+            _build(node.left, depth - 1, _x1, _y1)
+            _build(node.right, depth - 1, _x2, _y2)
+
+        _build(self._tree.root, self.max_depth, x, y)
+
+    def predict(self, x: np.ndarray) -> np.ndarray:
+        def _predict(node, _x):
+            _col, _split, _res_value = node.value
+            if node.left.value is None and node.right.value is None:
+                return _res_value
+
+            elif node.left.value is not None and node.right.value is not None:
+                if _x[_col] < _split:
+                    return _predict(node.left, _x)
+                else:
+                    return _predict(node.right, _x)
+            elif node.left.value is None and node.right.value is not None:
+                return _predict(node.right, _x)
+            elif node.left.value is not None and node.right.value is None:
+                return _predict(node.left, _x)
+
+        res = [_predict(self._tree.root, x[i, :]) for i in range(len(x))]
+        return np.array(res)
