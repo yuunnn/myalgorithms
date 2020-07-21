1ea4d0aed794f7fbbbfb39189fe122a62ef6c732
yuunnn <wooxy610@icloud.com>
2020-07-21 20:52:08 +0800

    用rnn拟合sin曲线（个人认为还是比较完美的）

--- test/nn_algs/test_rnn_sin.py
+++ test/nn_algs/test_rnn_sin.py
@@ -3,25 +3,28 @@ from algs.nn_algoritms.common.layer import Dense, SimpleRNN
 
 import numpy as np
 
-data = np.linspace(-10, 10, 10000)
+data = np.linspace(-10, 10, 2000)
 data = np.sin(data)
 
 x = []
 y = []
 for i in range(1000):
-    x.append(data[i:i + 100])
-    y.append(data[i + 1:i + 101])
+    x.append(data[i:i + 50])
+    y.append(data[i + 1:i + 51])
 
-x = np.array(x).reshape(1000, 100, 1)
-y = np.array(y).reshape(1000, 100, 1)
+x = np.array(x).reshape(1000, 50, 1)
+y = np.array(y).reshape(1000, 50, 1)
 
-model = Model(lr=0.002, max_iter=2000, loss="mse2d")
+model = Model(lr=0.02, max_iter=500, loss="mse2d")
 model.add(SimpleRNN(hidden_activation='tanh',
                     output_activation='linear',
                     max_length=1,
-                    hiddenDimension=200,
+                    hiddenDimension=10,
                     outputsDimension=1))
-model.fit(x, y, watch_loss=False)
+model.fit(x, y, watch_loss=True)
+y_pre = model.predict(x)
+y_pre = y_pre[:, -1, :]
 
 import matplotlib.pyplot as plt
-plt.plot(model.history['loss'])
+
+plt.plot(y_pre)
