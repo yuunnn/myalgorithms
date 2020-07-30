34e7f8a3f459c07dd0cc1724291ffda89f6a6227
yuunnn <wooxy610@icloud.com>
2020-07-30 18:55:42 +0800

    增加模型保存和读取的功能

--- test/nn_algs/test_xor.py
+++ test/nn_algs/test_xor.py
@@ -1,11 +1,12 @@
 from algs.nn_algoritms.common.model import Model
 from algs.nn_algoritms.common.layer import Dense
+from algs.nn_algoritms.common.utils import save_model, load_model
 import numpy as np
 
 x = np.array([[0, 1], [1, 1], [0, 0], [0, 1]])
 y = np.array([1, 0, 0, 1])
 
-model = Model(lr=0.02, epoch=10000, loss="Crossentropy_with_softmax")
+model = Model(lr=0.02, epoch=10000, loss="Crossentropy_with_softmax", classes=2)
 model.add(Dense(activation='leakyrelu', units=50))
 model.add(Dense(activation='tanh', units=25))
 model.add(Dense(activation='softmax', units=2))
@@ -13,4 +14,8 @@ model.fit(x, y)
 print(model.predict(x))
 
 import matplotlib.pyplot as plt
+
 plt.plot(model.history['loss'])
+
+save_model(model, "./test.m")
+model = load_model("./test.m")
