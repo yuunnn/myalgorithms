45542d7264498d531108487b16e58721499a7e63
yuunnn <wooxy610@icloud.com>
2020-07-30 19:11:51 +0800

    nn支持gpu加速了！但请确保你已经安装了cupy

--- algs/nn_algoritms_gpu/common/loss.py
+++ algs/nn_algoritms_gpu/common/loss.py
@@ -1,4 +1,4 @@
-import numpy as np
+import cupy as np
 from abc import ABC
 
 
--- test/nn_algs/test_cnn_gpu.py
+++ test/nn_algs/test_cnn_gpu.py
@@ -1,10 +1,10 @@
 import warnings
 import cupy as np
-from algs.nn_algoritms.common.model import Model
-from algs.nn_algoritms.common.layer import Dense, Conv2d, Flatten
+from algs.nn_algoritms_gpu.common.model import Model
+from algs.nn_algoritms_gpu.common.layer import Dense, Conv2d, Flatten
 from tensorflow.keras.datasets import cifar10
-warnings.filterwarnings("ignore")
 
+warnings.filterwarnings("ignore")
 
 (train_images, train_labels), (test_images, test_labels) = cifar10.load_data()
 train_images = np.array(train_images)
@@ -12,15 +12,18 @@ train_labels = np.array(train_labels)
 test_images = np.array(test_images)
 test_labels = np.array(test_labels)
 
-
 train_images = (train_images / 255).astype('float16').transpose([0, 3, 2, 1])
 test_images = (test_images / 255).astype('float16').transpose([0, 3, 2, 1])
 
-model = Model(lr=0.05, epoch=250, loss="Crossentropy_with_softmax", classes=10,
+model = Model(lr=0.02, epoch=60, loss="Crossentropy_with_softmax", classes=10,
               optimizer='sgd_with_momentum', decay=0.9999,
-              early_stop=True, tol=2e-4, momentum_beta=0.9, batch_size=256, shuffle=1)
+              early_stop=True, tol=2e-4, momentum_beta=0.9, batch_size=1048, shuffle=1)
 model.add(Conv2d(activation='leakyrelu', units=16, kernel_size=[3, 3], strides=1, padding='valid'))
 model.add(Conv2d(activation='leakyrelu', units=32, kernel_size=[3, 3], strides=1, padding='valid'))
 model.add(Flatten(input_shape=4))
 model.add(Dense(activation='softmax', units=10))
 model.fit(train_images, train_labels, watch_loss=1)
+
+from algs.nn_algoritms_gpu.common.utils import save_model
+
+save_model(model, "./test.m")
